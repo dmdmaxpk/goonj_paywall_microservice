5e8981d9de576bace25f8005df4158b15770b095
Farhan <farhan.arid1352@gmail.com>
2019-12-16 11:45:32 +0500

    commit ot test

--- app.js
+++ app.js
@@ -40,12 +40,9 @@ rabbitMq.initializeMesssageServer((err, channel) => {
         //Let's start queue consumption
         // Messaging Queue
         rabbitMq.consumeQueue(config.queueNames.messageDispathcer, (response) => {
-            console.log(response.content);
             let messageObj = JSON.parse(response.content);
-            console.log(messageObj);
-
             billingRepo.sendMessage(messageObj.message, messageObj.msisdn).then(data => {
-                console.log(data.message);
+                console.log(data);
             }).catch(error => {
                 console.log('Error: ', error.message)
             });
