befc3cd3cb9748d39f91e8810e7342e0906d13ce
Farhan <farhan.arid1352@gmail.com>
2019-12-20 17:21:55 +0500

    -

--- app.js
+++ app.js
@@ -210,8 +210,8 @@ billingRepo.generateToken().then(async(token) => {
                 
                 // Subscriptin Queue
                 rabbitMq.consumeQueue(config.queueNames.subscriptionDispatcher, (response) => {
-                    rabbitMq.acknowledge(response);
-                    //consumeSusbcriptionQueue(response);
+                    //rabbitMq.acknowledge(response);
+                    consumeSusbcriptionQueue(response);
                 });
             }
         });
