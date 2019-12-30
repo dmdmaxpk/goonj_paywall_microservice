07cc8ab427ef903f814c65fdb150deee15ef0e70
Farhan <farhan.arid1352@gmail.com>
2019-12-30 13:38:34 +0500

    Status no subscriber case handled

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -296,7 +296,9 @@ exports.status = async (req, res) => {
 	if(user){
 		let result = await subscriberRepo.getSubscriber(user._id);
 		if(result){
-			res.send({code: config.code_success, data: result});	
+			res.send({code: config.codes.code_success, data: result});	
+		}else{
+			res.send({code: config.codes.code_error, data: 'No subscriber found.'});	
 		}
 	}else{
 		res.send({code: config.codes.code_error, message: 'Invalid msisdn provided.'});
