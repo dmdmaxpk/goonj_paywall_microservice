914d8457ed7aa1d1e75e275978a23b9536d7ef20
Farhan Ali <farhan.ali@dmdmax.com>
2020-01-23 15:20:20 +0500

    -

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use((req, res, next) => {
     if (req.path.includes("/cron/")){
         next();
     } else {
-        app.use(logger('dev'));
+        logger('dev');
         next();
     }
 });
