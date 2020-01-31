1143eb5e5160cff804a19ba215e73a707e7b107f
Farhan Ali <farhan.ali@dmdmax.com>
2020-01-31 12:24:35 +0500

    -

--- app.js
+++ app.js
@@ -222,7 +222,7 @@ consumeSusbcriptionQueue = async(res) => {
                         }
                     }).catch(async (error) => {
                         console.log('Error:', error.message);
-                        console.log('Error Data - ', error.data);
+                        console.log('Error Data - ', error.request.response);
                         if (error.message === "Request failed with status code 500") {
                             // TPS exceeded, noAcknowledge will requeue this record.
                             console.log('Sending back to queue');
