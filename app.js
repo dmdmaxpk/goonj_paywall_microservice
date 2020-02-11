3c3e894c3816d549167e005dd502d8349b666e9f
Hamza <hamzashujaat218@gmail.com>
2020-02-11 21:37:44 +0500

    .

--- app.js
+++ app.js
@@ -24,6 +24,8 @@ require('./models/ViewLog');
 var RabbitMq = require('./repos/queue/RabbitMq');
 var billingRepo = require('./repos/BillingRepo');
 var tpsCountRepo = require('./repos/tpsCountRepo');
+var reportsRepo = require('./repos/ReportsRepo');
+reportsRepo.dailyReport();
 
 const app = express();
 
--- repos/ReportsRepo.js
+++ repos/ReportsRepo.js
@@ -9,7 +9,10 @@ const csvWriter = createCsvWriter({
         {id: 'date', title: 'Date'},
         {id: 'newUser', title: 'New User'},
         {id: 'newSubscriber', title: 'New Subcriber'},
-        {id: 'revenue', title: 'Revenue'}
+        {id: 'trials', title: 'Trials Activated'},
+        {id: 'users_billed', title: 'Users Billed'},
+        {id: 'revenue', title: 'Revenue'},
+
     ]
 });
 var nodemailer = require('nodemailer');
@@ -37,13 +40,21 @@ dailyReport = async() => {
         {$sort: {"date": -1}} 
     ]);
 
-    let billingHistoryStats = await BillingHistory.aggregate([
+    let billingStats = await BillingHistory.aggregate([
             { $match: { "billing_status": "Success" } },
             {$group: {_id: {"day": {"$dayOfMonth" : "$billing_dtm"}, "month": { "$month" : "$billing_dtm" },
-                "year":{ $year: "$billing_dtm" } } , revenue:{ $sum: "$price" } } },
+                "year":{ $year: "$billing_dtm" } } , revenue:{ $sum: "$price" },count:{$sum: 1} } },
             {$project: {  "date":{"$dateFromParts":{ year: "$_id.year","month":"$_id.month","day":"$_id.day" }}, 
-                "revenue": "$revenue",_id:-1 }},{$sort: {"date": -1}}
-        ]);
+                "revenue": "$revenue","count":"$count",_id:-1 }},{$sort: {"date": -1}}
+        ]);       
+    
+    let trialStats = await BillingHistory.aggregate([
+        { $match: { "billing_status": "trial" } },
+        {$group: {_id: {"day": {"$dayOfMonth" : "$billing_dtm"}, "month": { "$month" : "$billing_dtm" },
+            "year":{ $year: "$billing_dtm" } } , trials:{ $sum: 1 } } },
+        {$project: {  "date":{"$dateFromParts":{ year: "$_id.year","month":"$_id.month","day":"$_id.day" }}, 
+            "trials": "$trials",_id:-1 }},{$sort: {"date": -1}}
+    ]);    
 
     let resultToWrite= {};
     userStats.forEach(userStat => {
@@ -58,35 +69,46 @@ dailyReport = async() => {
         resultToWrite[subsc.date.toDateString()]['newSubscriber'] = subsc.count;
     });
 
-    billingHistoryStats.forEach(billingHistor => {
+    billingStats.forEach(billingHistor => {
         console.log(billingHistor.date.toDateString());
         if(resultToWrite[billingHistor.date.toDateString()]) {
             resultToWrite[billingHistor.date.toDateString()]['revenue'] = billingHistor.revenue;
+            resultToWrite[billingHistor.date.toDateString()]['users_billed'] = billingHistor.count;
+        }
+    });
+
+    trialStats.forEach(trialStat => {
+        console.log(trialStat.date.toDateString());
+        if(resultToWrite[trialStat.date.toDateString()]) {
+            resultToWrite[trialStat.date.toDateString()]['trials'] = trialStat.trials;
         }
     });
 
     let resultToWriteToCsv= [];
     for (res in resultToWrite) {
         let temp = {date: res, newUser: resultToWrite[res].newUser , newSubscriber: resultToWrite[res].newSubscriber,
-            revenue: resultToWrite[res].revenue }
+            revenue: resultToWrite[res].revenue, users_billed: resultToWrite[res].users_billed, trials: resultToWrite[res].trials }
         resultToWriteToCsv.push(temp);
     } 
 
     try {
-        await csvWriter.writeRecords(resultToWriteToCsv)
-        var info = await transporter.sendMail({
-            from: 'paywall@dmdmax.com.pk', // sender address
-            to: "paywall@dmdmax.com.pk", // list of receivers
-            subject: `PayWall Report ${(new Date()).toDateString()}`, // Subject line
-            text: `PFA some basic stats for Paywall. `, // plain text bodyday
-            attachments:[
-                {
-                    filename: "report.csv",
-                    path: "./report.csv"
-                }
-            ]
-        });
-        console.log("info",info);
+        csvWriter.writeRecords(resultToWriteToCsv).then(async (data) => {
+            var info = await transporter.sendMail({
+                from: 'paywall@dmdmax.com.pk', // sender address
+                to: ["paywall@dmdmax.com.pk"], // list of receivers
+                subject: `PayWall Report ${(new Date()).toDateString()}`, // Subject line
+                text: `PFA some basic stats for Paywall. `, // plain text bodyday
+                attachments:[
+                    {
+                        filename: "report.csv",
+                        path: "./report.csv"
+                    }
+                ]
+            });
+            console.log("info",info);
+        }).catch(er => {
+            console.log("err",er)
+        })
     } catch(err) {
         console.log(err);
     }
