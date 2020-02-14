27cb7c21c1d786f293d1bcd92e1cc4d171408599
Hamza <hamzashujaat218@gmail.com>
2020-02-14 18:30:14 +0500

    config error fixed

--- config.js
+++ config.js
@@ -46,7 +46,7 @@ const time_between_billing_attempts_hours = 8;
 let config = {
     development: {
         port: '5000',
-        mongoDB: 'mongodb://localhost:27017/goonjpaywall2',
+        mongoDB: 'mongodb://localhost:27017/goonjpaywall',
         rabbitMq: 'amqp://localhost',
         queueNames: queueNames,
         telenor_dcb_api_baseurl: telenor_dcb_api_baseurl,
