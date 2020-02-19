947c3d1d3463e98b6acb263360b2e38c2eda9930
Farhan Ali <farhan.ali@dmdmax.com>
2020-02-19 15:14:42 +0500

    -

--- app.js
+++ app.js
@@ -340,7 +340,7 @@ async function assignGracePeriodToSubscriber(subscriber,user_id){
                 console.log("[assignGracePeriodToSubscriber][not_billed][else]", subObj.user_id);
             
                 //Send acknowldement to user
-                let message = 'You have insufficient balance for Goonj TV, please try again after recharge. Thanks;
+                let message = 'You have insufficient balance for Goonj TV, please try again after recharge. Thanks';
                 await billingRepo.sendMessage(message, user.msisdn);
             }
             subObj.consecutive_successive_bill_counts = 0;
