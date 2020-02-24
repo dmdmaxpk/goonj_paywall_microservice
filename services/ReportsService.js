5fdf0a44885b291f2c590845e0c37eb44f006056
Hamza <hamzashujaat218@gmail.com>
2020-02-24 17:50:54 +0500

    Automation of Report to send Ideation

--- services/ReportsService.js
+++ services/ReportsService.js
@@ -3,6 +3,8 @@ const reportsRepo = require('../repos/ReportsRepo');
 generateDailyReport = async() => {
     console.log("generateDailyReport");
     reportsRepo.dailyReport();
+
+    reportsRepo.callBacksReport();
 }
 
 module.exports = {
