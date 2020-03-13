e627a2a2abef69f93a8ea5b03417444529e6b0e4
Farhan Ali <farhan.ali@dmdmax.com>
2020-03-13 17:22:14 +0500

    Changes made for testing

--- repos/queue/consumers/FreeMbsConsumer.js
+++ repos/queue/consumers/FreeMbsConsumer.js
@@ -57,6 +57,10 @@ function getCurrentDate() {
     return strDateTime;
 }
 
+function AddZero(num) {
+    return (num >= 0 && num < 10) ? "0" + num : num + "";
+}
+
 module.exports = {
     subscribeFreeMbs: subscribeFreeMbs
 }
