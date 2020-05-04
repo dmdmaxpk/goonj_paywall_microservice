88c5a7bba1c37ae0569f2e782d29b5c2c1481b80
Hamza <hamzashujaat218@gmail.com>
2020-05-04 18:05:32 +0500

    is_billable_in_this_cycle set to false in subscription_renewal expiry

--- services/SubscriptionRenewalService.js
+++ services/SubscriptionRenewalService.js
@@ -34,7 +34,7 @@ subscriptionRenewal = async() => {
         for(let i = 0; i < subscribersNotToRenew.length; i++) {
             let subscriber = subscribersNotToRenew[i];
             await userRepo.updateUserById(subscriber.user_id, {subscription_status: 'expired'});
-            let sub = await subsriberRepo.updateSubscriber(subscriber.user_id,{subscription_status: 'expired',is_allowed_to_stream:false});
+            let sub = await subsriberRepo.updateSubscriber(subscriber.user_id,{subscription_status: 'expired',is_allowed_to_stream:false,is_billable_in_this_cycle:false});
             let user = await userRepo.getUserById(subscriber.user_id);
             let billingHistory = {};
 		    billingHistory.user_id = subscriber.user_id;
