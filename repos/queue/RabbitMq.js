7d4c640fec1211152a3d918141e2593a44ab7821
Hamza <hamzashujaat218@gmail.com>
2020-06-12 23:30:29 +0500

    .

--- repos/queue/RabbitMq.js
+++ repos/queue/RabbitMq.js
@@ -72,6 +72,8 @@ class RabbitMq {
     }
 
     noAcknowledge(message){
+        console.log("[noAcknowledge][message]",message);
+        console.log("[noAcknowledge][channel]",this.channel);
         this.channel.nack(message);
     }
 
