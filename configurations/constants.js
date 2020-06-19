3d41cf0d066d1b905affa446cdd112ea1fd21827
Hamza <hamzashujaat218@gmail.com>
2020-06-20 03:35:33 +0500

    .

--- configurations/constants.js
+++ configurations/constants.js
@@ -6,7 +6,7 @@ class Constants {
             QDfG: `Apka Goonjtv Free  %trial_hours%hrs trial on kr dia gya ha. Phla charge kal hoga. Service khatm krna k liye %unsub_link%`,                            
             QDfH: `Apka Goonj comedy Portal activate kar dia gya hai. Service khatam karnay ke liye %unsub_link%`,
             QDfI: `Apka Goonj comedy Portal activate kar dia gya hai. Service khatam karnay ke liye %unsub_link%`,
-            gdn:`Apko Goonj TV activate kr dia gaya ha. Jub chahien Jaib se Mobile nikalien aur TOP LIVE TV Channels deikhen siraf 5 Ruppey/d main.`
+            gdn:`Apko Goonj TV activate kr dia gaya ha. Jub chahien Jaib se Mobile nikalien aur TOP LIVE TV Channels deikhen.`
         }
     }
 
