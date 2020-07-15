25625c0037954cf5ea6dd1a82ec5ea4bbb244840
Hamza <hamzashujaat218@gmail.com>
2020-07-15 10:32:17 +0500

    WIP: sms text changing

--- configurations/constants.js
+++ configurations/constants.js
@@ -2,17 +2,20 @@
 class Constants {
     constructor({}) {
         this.subscription_messages = {
-            QDfC: `Apka Goonjtv Free %trial_hours%hrs trial on kr dia gya ha. Phla charge kal hoga. Service khatm krna k liye %unsub_link%`,
-            QDfG: `Apka Goonjtv Free %trial_hours%hrs trial on kr dia gya ha. Phla charge kal hoga. Service khatm krna k liye %unsub_link%`,                            
+            QDfC: `Apka Goonjtv Free %trial_hours% hour free trial dey dia gaya ha. Jub chahien Jaib se Mobile nikalien aur TOP 
+                    LIVE TV Channels deikhen siraf %price% Ruppey/d main.`,
+            QDfG: `Apko Goonj TV %trial_hours% hour free trial dey dia gaya ha. Jub chahien Jaib se Mobile nikalien aur TOP LIVE TV Channels deikhen siraf %price%
+                         Ruppey/d main.`,                            
             QDfH: `Apka Goonj comedy Portal activate kar dia gya hai. Service khatam karnay ke liye %unsub_link%`,
             QDfI: `Apka Goonj comedy Portal activate kar dia gya hai. Service khatam karnay ke liye %unsub_link%`,
             gdn:`Apko Goonj TV activate kr dia gaya ha. Jub chahien Jaib se Mobile nikalien aur TOP LIVE TV Channels deikhen.`
         },
         this.message_after_repeated_succes_charge = {
-            QDfC: `Goonj tv per top channels Rs%price%/day dekhnay ka shukriya. Service istemal ke liye www.goonj.pk aur service khatam karnay ke liye %unsub_link%`,
-            QDfG: `Goonj tv per top channels Rs%price%/week dekhnay ka shukriya. Service istemal ke liye www.goonj.pk aur service khatam karnay ke liye %unsub_link%`,                            
-            QDfH: `Goonj tv pr Comedy Videos Rs%price%/day dekhnay ka shukriya. Service istemal ke liye www.goonj.pk aur service khatam karnay ke liye %unsub_link%`,
-            QDfI: `Goonj tv pr Comedy Videos Rs%price%/week dekhnay ka shukriya. Service istemal ke liye www.goonj.pk aur service khatam karnay ke liye %unsub_link%`
+            
+            QDfC: `Thank you for using Goonj TV with Live TV Only at Rs. %price%/day. Goonj TV Deikhnay k liay ya package ki tabdeli k liay click karein.`,
+            QDfG: `Thank you for using Goonj TV with Live TV Only at Rs. %price%/week. Goonj TV Deikhnay k liay ya package ki tabdeli k liay click karein.`,                            
+            QDfH: `Thank you for using Goonj TV Comedy Portal Only at Rs. %price%/day. Goonj TV Deikhnay k liay ya package ki tabdeli k liay click karein.`,
+            QDfI: `Thank you for using Goonj TV Comedy Portal Only at Rs. %price%/week. Goonj TV Deikhnay k liay ya package ki tabdeli k liay click karein.`
         }
     }
 
--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -840,7 +840,7 @@ exports.unsubscribe = async (req, res) => {
 				result = await billingHistoryRepo.createBillingHistory(history);
 
 				// send SMS to user
-				let smsText = `Apki Goonj TV per ${packageObj.package_name} ki subscription khatm kr di gai ha. Phr se subscribe krne k lye link par click karen https://www.goonj.pk/goonjplus/subscribe`;
+				let smsText = `You package to Goonj TV has expired, click below link to subscribe again www.goonj.pk/goonjplus/subscribe`;
 				messageRepo.sendSmsToUser(smsText,user.msisdn);
 
 				if(result){
