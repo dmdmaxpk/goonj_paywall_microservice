c930ad894218ad6dc49de69fa77a5a6cd4fcae79
Farhan Ali <farhan.ali@dmdmax.com>
2020-07-20 15:27:58 +0500

    added contructor variables

--- services/PaymentProcessService.js
+++ services/PaymentProcessService.js
@@ -2,6 +2,8 @@ const config = require('../config');
 
 class PaymentProcessService {
     constructor({billingRepository, easypaisaPaymentService}){
+        this.billingRepository = billingRepository;
+        this.PaymentProcessService = PaymentProcessService;
     }
 
     async fullChargeAttempt(msisdn, packageObj, transaction_id, subscription){
