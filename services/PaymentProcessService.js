603c8bb9fadc953c4874006e9a6a49e782513b31
Azam ali <azam.mamngo@gmail.com>
2020-07-20 15:39:23 +0500

    payment service add block for easypaisa transaction

--- services/PaymentProcessService.js
+++ services/PaymentProcessService.js
@@ -4,11 +4,16 @@ class PaymentProcessService {
     constructor({billingRepository, easypaisaPaymentService}){
         this.billingRepository = billingRepository;
         this.PaymentProcessService = PaymentProcessService;
+        this.easypaisaPaymentService = easypaisaPaymentService;
     }
 
     async fullChargeAttempt(msisdn, packageObj, transaction_id, subscription){
 
+        if (subscription.payment_source_id){
+            let record = await this.easypaisaPaymentService.bootTransactionScript(requestData.msisdn, requestData.amount, easypaisaToken, requestData.opt);
+
+        }
     }
 }
 
-module.exports = PaymentProcessService
+module.exports = PaymentProcessService;
