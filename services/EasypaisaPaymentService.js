d21b1202c3bf5256191fb398aad648658870dc8f
Farhan Ali <farhan.ali@dmdmax.com>
2020-07-21 16:37:54 +0500

    rsa sign added

--- services/EasypaisaPaymentService.js
+++ services/EasypaisaPaymentService.js
@@ -288,8 +288,8 @@ class EasypaisaPaymentService {
                 .update(JSON.stringify(object.request))
                 .digest('sha256');
 
-            console.log('generateSignature - hash: ', hash);
-            this.signature = hash;
+            console.log('generateSignature - hash: ', hash.toString('base64'));
+            this.signature = hash.toString('base64');
             return {'code': config.codes.code_success, 'message': 'Signature is generated successfully', 'method': 'generateSignature'};
         } catch(err){
             return {'code': config.codes.code_error, 'message': err.message, 'method': 'generateSignature'};
