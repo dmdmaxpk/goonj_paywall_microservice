daf7f0a0b72d8df67a1fc86f3c1aff8ed1df3b69
Farhan Ali <farhan.ali@dmdmax.com>
2020-07-23 12:54:29 +0500

    switch payment

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -992,6 +992,17 @@ exports.switchPaymentSource = async (req, res) => {
 				}
 			}else if(new_source === 'easypaisa'){
 				// create link transaction with easypaisa
+				if(record.ep_token === undefined){
+					// no ep_token available
+				}else{
+					// update record
+					let result = await subscriptionRepo.updateSubscription(subscription_id, record);
+					if (result === undefined){
+						res.send({code: config.codes.code_success, message: 'Payment source updated successfully', gw_transaction_id: gw_transaction_id});
+					}else{
+						res.send({code: config.codes.code_error, message: 'Failed to updated payment source.', gw_transaction_id: gw_transaction_id});
+					}
+				}
 			}
         } else{
             res.send({code: config.codes.code_error, message: 'Payment source should be different.', gw_transaction_id: gw_transaction_id});
