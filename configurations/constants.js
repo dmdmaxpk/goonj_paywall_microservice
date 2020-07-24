dd3e4091e79047b56ba7d60d8a91383227ad0c92
Hamza <hamzashujaat218@gmail.com>
2020-07-24 22:34:25 +0500

    changing sms text

--- configurations/constants.js
+++ configurations/constants.js
@@ -2,28 +2,29 @@
 class Constants {
     constructor({}) {
         this.subscription_messages = {
-            QDfC: `Apko Goonj Tv Free %trial_hours% hour trial dey dia gaya ha. Jub chahien Jaib se Mobile nikalien aur TOP  LIVE TV Channels deikhen siraf %price% Ruppey/d main.`,
-            QDfG: `Apko Goonj TV %trial_hours% hour free trial dey dia gaya ha. Jub chahien Jaib se Mobile nikalien aur TOP LIVE TV Channels deikhen siraf %price% Ruppey/d main.`,                            
+            
+            QDfC: `Free %trial_hours%hrs Goonj tv trial activate kar dia gya hai. Top channels mobile par dekhnay ke liye www.goonj.pk/live aur numainday se baat ke lye 727200 milayein`,
+            QDfG: `Free %trial_hours%hrs Goonj tv trial activate kar dia gya hai. Top channels mobile par dekhnay ke liye www.goonj.pk/live aur numainday se baat ke lye 727200 milayein`,                            
             QDfH: `Your Goonj TV subscription for Comedy Portal has been activated at Rs.%price%/day. Thankyou for watching Goonj Comedy`,
             QDfI: `Your Goonj TV subscription for Comedy Portal has been activated at Rs.%price%/week. Thankyou for watching Goonj Comedy`,
             gdn:`Apko Goonj TV activate kr dia gaya ha. Jub chahien Jaib se Mobile nikalien aur TOP LIVE TV Channels deikhen.`
         },
         this.subscription_messages_direct = {
-            QDfC: `Thank you for using Goonj TV with Live TV Only at Rs. %price%/day. Goonj TV Deikhnay k liay ya package ki tabdeli k liay click karein http://www.goonj.pk/live`,
-            QDfG: `Thank you for using Goonj TV with Live TV Only at Rs. %price%/week. Goonj TV Deikhnay k liay ya package ki tabdeli k liay click karein http://www.goonj.pk/live`,
-            QDfH: `Your Goonj TV subscription for Comedy Portal has been activated at Rs.%price%/day. Thankyou for watching Goonj Comedy`,
-            QDfI: `Your Goonj TV subscription for Comedy Portal has been activated at Rs.%price%/week. Thankyou for watching Goonj Comedy`,
+            QDfC: `Goonj Live TV Rs %price%/wk subscribe kar diya gya hai. TV dekhnay k liye http://www.goonj.pk/live aur numainday se baat k liye 727200 milayein`,
+            QDfG: `Goonj Live TV Rs%price%/d subscribe kar diya gya hai. TV dekhnay k liye http://www.goonj.pk/live aur numainday se baat k liye 727200 milayein`,
+            QDfH: `Goonj Comedy Rs%price%/+t/d mein subscribe kar di gaye hai. Service dekhne k liye http://www.goonj.pk/ aur numainday se baat ke liye 727200 milayein.`,
+            QDfI: `Goonj Comedy Rs%price%/+t/w mein subscribe kar di gaye hai. Service dekhne k liye http://www.goonj.pk/ aur numainday se baat ke liye 727200 milayein.`,
         },
         this.message_after_repeated_succes_charge = {
-            QDfC: `Thank you for using Goonj TV with Live TV Only at Rs. %price%/day. Goonj TV Deikhnay k liay ya package ki tabdeli k liay click karein. http://www.goonj.pk/live`,
-            QDfG: `Thank you for using Goonj TV with Live TV Only at Rs. %price%/week. Goonj TV Deikhnay k liay ya package ki tabdeli k liay click karein. http://www.goonj.pk/live`,                            
-            QDfH: `Thank you for using Goonj TV Comedy Portal Only at Rs. %price%. Goonj TV Deikhnay k liay ya package ki tabdeli k liay click karein. http://www.goonj.pk/live`,
-            QDfI: `Thank you for using Goonj TV Comedy Portal Only at Rs. %price%. Goonj TV Deikhnay k liay ya package ki tabdeli k liay click karein. http://www.goonj.pk/live`
+            QDfC: `Goonj Live TV Rs. 5/day dekhnay ka shukriya.. Goonj TV Deikhnay k liay ya package ki tabdeli k liay click karein. http://www.goonj.pk/live`,
+            QDfG: `Goonj tv dekhny ka shukriya. Goonj Tv dekhnay k liay ya package ki tabdeli k liay click karein. http://www.goonj.pk/live`,                            
+            QDfH: `Ap Pakistan ki best Comedy Portal service istamal kar rahey hain. Service Deikhnay ya Package ki tabdeeli k liay click karein. http://www.goonj.pk/live`,
+            QDfI: `Ap Pakistan ki best Comedy Portal service istamal kar rahey hain. Service Deikhnay ya Package ki tabdeeli k liay click karein. http://www.goonj.pk/live`
         }
 
         this.message_after_first_successful_charge = {
-            QDfC: `Ap Pakistan ki best Live TV service istamal kar rahey hain. Service Deikhnay ya Package ki tabdeeli k liay click karein. http://www.goonj.pk/live`,
-            QDfG: `Ap Pakistan ki best Live TV service istamal kar rahey hain. Service Deikhnay ya Package ki tabdeeli k liay click karein. http://www.goonj.pk/live`,                            
+            QDfC: `Ap Pakistan ki best Live TV service istamal kar rahey hain. Live tv dekhy k liye www.goonj.pk/live aur numainday se baat k liye 727200 milayein `,
+            QDfG: `Ap Pakistan ki best Live TV service istamal kar rahey hain. Live tv dekhy k liye www.goonj.pk/live aur numainday se baat k liye 727200 milayein`,                            
             QDfH: `Ap Pakistan ki best Comedy Portal service istamal kar rahey hain. Service Deikhnay ya Package ki tabdeeli k liay click karein. http://www.goonj.pk/live`,
             QDfI: `Ap Pakistan ki best Comedy Portal service istamal kar rahey hain. Service Deikhnay ya Package ki tabdeeli k liay click karein. http://www.goonj.pk/live`
         }
