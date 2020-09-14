461429fd2a21d23a1c888f9d05181ba239d2a1b9
Farhan Ali <farhan.ali@dmdmax.com>
2020-09-14 14:25:27 +0500

    chunks limit increased to 1600 from 8800 as tps increased

--- repos/SubscriptionRepo.js
+++ repos/SubscriptionRepo.js
@@ -80,7 +80,7 @@ class SubscriptionRepository {
     }
     
     async getRenewableSubscriptions  ()  {
-        let results = await Subscription.find({is_billable_in_this_cycle: true, active: true}).sort({priority:1}).limit(8800);
+        let results = await Subscription.find({is_billable_in_this_cycle: true, active: true}).sort({priority:1}).limit(16000);
         return results;
     }
     
