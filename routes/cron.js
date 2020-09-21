12279a7a9931e5ceb5f6cf4e2c0ce25462d69f81
Muhammad <muhammad.azam@dmdmax.com>
2020-09-21 10:20:17 +0500

    send opt issue - paywall cron routes enable

--- routes/cron.js
+++ routes/cron.js
@@ -5,13 +5,11 @@ const migrationService = require('../services/SubscriberToSubscriptionMigration'
 
 //
 
-/*
 router.route('/markRenewableUsers')
     .get(controller.markRenewableUsers);
 
 router.route('/subscriptionRenewal')
     .get(controller.subscriptionRenewal);
-*/
 
 // router.route('/postPSLUserMigration')
 //     .get(controller.postPSLUserMigration);
@@ -22,17 +20,14 @@ router.route('/subscriptionRenewal')
 router.route('/tokenRefresh')
     .get(controller.refreshToken);
 
-/*
 router.route('/addInBillingQueue')
     .get(controller.addInBillingQueue);
-*/
 
 router.route('/tpsCountReset')
     .get(controller.tpsCountReset);
 
 router.route('/dailyAmoutReset')
     .get(controller.dailyAmoutReset);
-/*
 
 router.route('/checkLastSeenOfUsers')
     .get(controller.checkLastSeenOfUsers);
@@ -61,5 +56,4 @@ router.route('/migrate')
 router.route('/removemsisdns')
     .get(controller.removeDuplicateMsisdns);
 
-*/
 module.exports = router;
