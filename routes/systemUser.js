dc189edc6e9c126abfbe9b7e8ac445eab747ff48
Farhan Ali <farhan.ali@dmdmax.com>
2020-09-22 14:18:27 +0500

    -

--- routes/systemUser.js
+++ routes/systemUser.js
@@ -4,10 +4,7 @@ const controller = require('../controllers/systemUserController');
 const authMiddleWare = require('../middlewares/auth.middleware');
 const aclMiddleWare = require('../middlewares/acl.middleware');
 
-router.route('/login')
-    .post( controller.login);
-
-router.route('/unsubscribe')
-    .post(authMiddleWare.checkToken, aclMiddleWare.checkRole, controller.unsubscribe);
+router.route('/login').post(controller.login);
+router.route('/unsubscribe').post(authMiddleWare.checkToken, aclMiddleWare.checkRole, controller.unsubscribe);
 
 module.exports = router;
