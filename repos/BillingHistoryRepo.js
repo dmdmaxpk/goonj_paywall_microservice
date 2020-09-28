e2c9a0d4a8d513b9f643cdece3195a3417be29bd
Farhan Ali <farhan.ali@dmdmax.com>
2020-09-28 14:58:30 +0500

    success and failed request count report

--- repos/BillingHistoryRepo.js
+++ repos/BillingHistoryRepo.js
@@ -105,7 +105,7 @@ class BillingHistoryRepository {
                     _id: "$billing_status",
                     count: {$sum:1}
                 }
-            }], {allowDiskUse:true});
+            }]);
         return data;
     }
     
