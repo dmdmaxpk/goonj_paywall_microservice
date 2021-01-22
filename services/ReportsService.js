5f9e88202d5b661a51068c6a0aff44c41e80930d
Muhammad Azam <muhammad.azam@dmdmax.com>
2021-01-22 15:04:55 +0500

    blacklist to users - source - a file which have msisdn list

--- services/ReportsService.js
+++ services/ReportsService.js
@@ -152,10 +152,10 @@ generateMonthlyReports =  async() => {
 
 generateRandomReports =  async() => {
     //reportsRepo.getOnlySubscriberIds("app", "2020-08-01T00:00:00.000Z", "2020-09-01T00:00:00.000Z");
-    reportsRepo.generateReportForAcquisitionSourceAndNoOfTimeUserBilled();
+    // reportsRepo.generateReportForAcquisitionSourceAndNoOfTimeUserBilled();
     //reportsRepo.getNextBillingDtm();
     //reportsRepo.getReportForHeOrWifi();
-    //reportsRepo.expireBaseAndBlackList();
+    reportsRepo.expireBaseAndBlackList();
 }
 
 function sleep(ms) {
