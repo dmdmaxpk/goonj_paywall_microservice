d52971f58c2232b69fa86d9de8ca3bd1c9007027
Farhan Ali <farhan.ali@dmdmax.com>
2021-04-05 13:00:54 +0500

    a line removed which was creating error

--- services/SubscriptionService.js
+++ services/SubscriptionService.js
@@ -54,7 +54,6 @@ class SubscriptionService {
                             history.subscriber_id = subscription.subscriber_id;
                             history.subscription_id = subscription._id;
                             history.package_id = subscription.subscribed_package_id;
-                            history.paywall_id = paywall._id;
                             history.billing_status = 'expired';
                             history.source = source ? source : 'ccp_api';
                             history.operator = 'telenor';
