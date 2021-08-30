84e385e31636d41ef1c0b4d4bebb37397c6da68d
taha <taha@dmdmax.com>
2021-08-30 11:49:52 +0500

    routes commented

--- routes/index.js
+++ routes/index.js
@@ -2,29 +2,29 @@ const express = require('express');
 const router = express.Router();
 
 // Service Label
-router.get('/', (req, res) => res.send("User Microservice"));
+// router.get('/', (req, res) => res.send("User Microservice"));
 
-router.use('/auth', require('./authRoutes'));
+// router.use('/auth', require('./authRoutes'));
 
-router.use('/user',    require('./userRoutes'));
-router.use('/package',    require('./packageRoutes'));
-router.use('/otp',    require('./otpRoutes'));
+// router.use('/user',    require('./userRoutes'));
+// router.use('/package',    require('./packageRoutes'));
+// router.use('/otp',    require('./otpRoutes'));
 
-// Payment routes
-router.use('/payment',    require('./paymentRoute'));
+// // Payment routes
+// router.use('/payment',    require('./paymentRoute'));
 
-router.use('/report',    require('./reports'));
+// router.use('/report',    require('./reports'));
 
-router.use('/cron',    require('./cron'));
+// router.use('/cron',    require('./cron'));
 
-router.use('/paywall',    require('./paywall'));
+// router.use('/paywall',    require('./paywall'));
 
-router.use('/goonj',    require('./systemUser'));
+// router.use('/goonj',    require('./systemUser'));
 
-router.use('/ccd',    require('./ccd'));
+// router.use('/ccd',    require('./ccd'));
+
+// router.use('/billing-producer',    require('./billingProducerRoutes'));
 
 router.use('/subscriber',    require('./subscriberRoutes'));
-
-router.use('/billing-producer',    require('./billingProducerRoutes'));
 
 module.exports = router;
